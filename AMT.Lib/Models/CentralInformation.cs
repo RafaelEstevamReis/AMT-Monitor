67354352ab7d71bba4850e55c81fd1b4bebc8f02
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-19 23:19:03 -0300

    Models

---
+++ AMT.Lib/Models/CentralInformation.cs
@@ -0,0 +1,29 @@
+﻿namespace AMT.Lib.Models
+{
+    public class CentralInformation
+    {
+        public enum ConnectionType
+        {
+            Ethernet = 0x45, // 'E'
+            GPRS = 0x47, // 'G'
+            GPRS2 = 0x48, // 'H'
+        }
+
+        public ConnectionType Connection { get; set; }
+        public byte[] MacAddress { get; set; }
+        public byte[] PartialMacAddress { get; set; }
+        public int AccountId { get; set; }
+
+        public string PartialMacAddressString
+            => buildMac(PartialMacAddress);
+        public string MacAddressString
+            => buildMac(MacAddress);
+
+        private static string buildMac(byte[] macAddress)
+        {
+            if (macAddress is null) return null;
+            return string.Join(':', macAddress.Select(m => m.ToString("X2")));
+        }
+
+    }
+}
