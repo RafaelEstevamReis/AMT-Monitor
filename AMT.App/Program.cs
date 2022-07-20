567595430acbabc027e0f53577d9efdff93c042b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-19 23:19:20 -0300

    Create Program.cs

---
+++ AMT.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using AMT.Lib;
+using System.Text.Json;
+
+Console.WriteLine("START");
+
+var cnn = new Connection(9009);
+cnn.OnEvent += Cnn_OnEvent;
+cnn.OnMessage += (s, m) => Console.WriteLine(m);
+
+await cnn.StartAsync();
+
+
+void Cnn_OnEvent(object? sender, AMT.Lib.Models.EventInformation e)
+{
+    var json = JsonSerializer.Serialize(e);
+    Console.WriteLine(json); 
+}
