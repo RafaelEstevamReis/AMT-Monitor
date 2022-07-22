c817f97ff811706917376c6c52f1e8e16561fce4
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-21 23:38:23 -0300

    Create Connection.cs

---
+++ AMT.Lib/AMTPackets/Connection.cs
@@ -0,0 +1,20 @@
+﻿namespace AMT.Lib.AMTPackets
+{
+    public class Connection : DataPacket
+    {
+        public bool Success
+            => Data != null && Data.Length > 0 && Data[0] == 0;
+
+        public static DataPacket Request(byte[] password)
+        {
+            var data = new byte[9];
+            Buffer.BlockCopy(password, 0, data, 1, password.Length);
+            // captured a header and a trailer
+            data[0] = 1;
+            data[7] = 0x10;
+
+            return BuildPacket(Commands.CONNECT_PASSWORD, data);
+        }
+
+    }
+}
