1d4279b30bb9c50eee4ce67b6327e649d77750e1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-22 00:57:46 -0300

    Create EventPointer.cs

---
+++ AMT.Lib/AMTPackets/EventPointer.cs
@@ -0,0 +1,14 @@
+﻿namespace AMT.Lib.AMTPackets
+{
+    public class EventPointer: DataPacket
+    {
+        public int Index 
+            => ((Data[1] & 0xFF) << 8 | Data[2] & 0xFF) - 1;
+
+        public static DataPacket Request()
+        {
+            return BuildPacket(Commands.EVENT_POINTER, 0x00);
+        }
+
+    }
+}
