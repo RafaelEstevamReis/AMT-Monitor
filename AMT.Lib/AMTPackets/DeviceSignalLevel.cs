b465cbea0e3ca979104cad57a7686fe617403c05
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-06-29 00:45:26 -0300

    Device Ids

--- AMT.Lib/AMTPackets/DeviceSignalLevel.cs
+++ AMT.Lib/AMTPackets/DeviceSignalLevel.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Simple.AMT.AMTPackets
 {
     public class DeviceSignalLevel : DataPacket
     {
-        public static DataPacket Request()
+        public enum DeviceId : byte
         {
-            return BuildPacket(Commands.DEVICE_SIGNAL_LEVEL, 0x06);
+            Controller = 0,
+            Sensor = 1,
+            PGM = 6,
+            Keyboard = 9,
+            Siren = 10,
+            Repeater = 11,
+        }
+
+        public static DataPacket Request(DeviceId id, byte index)
+        {
+            if (id == DeviceId.Sensor)
+            {
+                return BuildPacket(Commands.DEVICE_SIGNAL_LEVEL, index);
+            }
+            else
+            {
+                byte deviceId = (byte)id;
+                return BuildPacket(Commands.DEVICE_SIGNAL_LEVEL, deviceId, index);
+            }
+        }
+
+        public override void Unpack(byte[] receivedBytes)
+        {
+            base.Unpack(receivedBytes);
+
+            receivedBytes = receivedBytes;
         }
     }
 }
