7290a924db9e59931d53c4f78831daacf9a4e8b8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-07-12 00:42:11 -0300

    Add controller basecode

---
+++ AMT.API/Controllers/CentralControllers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Simple.AMT;
+
+namespace AMT.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CentralControllers : ControllerBase
+    {
+        private readonly ILogger<CentralControllers> logger;
+        private readonly AMT8000 central;
+
+        public CentralControllers(ILogger<CentralControllers> logger, AMT8000 central)
+        {
+            this.logger = logger;
+            this.central = central;
+        }
+
+
+    }
+}
---
+++ AMT.API/Controllers/SensorsControllers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Simple.AMT;
+
+namespace AMT.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SensorsControllers : ControllerBase
+    {
+        private readonly ILogger<SensorsControllers> logger;
+        private readonly AMT8000 central;
+
+        public SensorsControllers(ILogger<SensorsControllers> logger, AMT8000 central)
+        {
+            this.logger = logger;
+            this.central = central;
+        }
+
+
+    }
+}
