67f5d40d4cdd8fdcbd45f9462e4ffb0d9cd75d0f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-07-12 00:40:21 -0300

    Create BasicController.cs

---
+++ AMT.API/Controllers/BasicController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Simple.AMT;
+
+namespace AMT.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasicController : ControllerBase
+    {
+        private readonly ILogger<BasicController> logger;
+        private readonly AMT8000 central;
+
+        public BasicController(ILogger<BasicController> logger, AMT8000 central)
+        {
+            this.logger = logger;
+            this.central = central;
+        }
+
+        [HttpGet]
+        public ActionResult IsConnected()
+        {
+            return Ok(central.IsConnected);
+        }
+
+    }
+}
