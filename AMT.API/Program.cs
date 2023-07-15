9ea0fb53a92a5c601fe444a4441fb486bb2826e8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-07-14 22:32:59 -0300

    Log more

--- AMT.API/Program.cs
+++ AMT.API/Program.cs
@@ -8,7 +8,7 @@ builder.Host.UseSerilog((context, configuration) =>
 {
     configuration
     //.MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", Serilog.Events.LogEventLevel.Warning)
+    //.MinimumLevel.Override("Microsoft.AspNetCore.Hosting", Serilog.Events.LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.AspNetCore.Infrastructure", Serilog.Events.LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.AspNetCore.Routing", Serilog.Events.LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", Serilog.Events.LogEventLevel.Warning)
